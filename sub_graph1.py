e519433762c3be53d4afa14bc5cd0e8b82ef327c
vedantiitkgp <vedantntpc@gmail.com>
2019-09-06 02:12:59 +0530

    Started working with degree of nodes

--- sub_graph1.py
+++ sub_graph1.py
@@ -1,7 +1,7 @@
 import snap 
 
 # Taking input from text file 
-G1 = snap.LoadEdgeList(snap.PNGraph,"/home/vedant/Social_computing/Social_Epinions/soc-Epinions1.txt",0,1)
+G1 = snap.LoadEdgeList(snap.PUNGraph,"/home/vedant/Social_computing/Social_Epinions/soc-Epinions1.txt",0,1)
 
 # Creating a new list with taking odd edges
 new_list = []
@@ -22,6 +22,22 @@ for i in range(len(new_list)):
 	v.Add(new_list[i])
 
 #Creating the subgraph
-soc_epinions1_subgraph = snap.GetSubGraph(G1,snap.TIntV.GetV(v))
+soc_epinions1_subgraph = snap.GetSubGraph(G1,v)
 
 print "Number of nodes in soc-Epinions1-subgraph: " + str(len(v))
+
+# Counting no of edges
+edge =0
+for i in soc_epinions1_subgraph.Edges():
+	edge = edge +1
+
+print "Number of edges in soc-Epinions1-subgraph: " + str(edge)
+
+# Getting the degree distribution
+
+CntV = snap.TIntPrV()
+
+# get degree distribution pairs (degree, count)
+snap.GetOutDegCnt(G1, CntV)
+
+print "Number of nodes with degree distribution "
